ef41634014ac7dcc734a8972d5f95918c675edd3
Sultan Achmad <sultangendut@gmail.com>
2020-12-02 09:52:07 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import puppeteer from 'puppeteer'
+import fs from 'fs/promises'
+
+let scrap = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.tokopedia.com/emas/harga-hari-ini/')
+    const date = await page.$eval('time', el => el.textContent)
+    const price = await page.$$eval('span.main-price', element =>
+        element.map(val => val.textContent)
+    )
+    await browser.close()
+
+    let gold = {
+        date: date,
+        buy: price[0],
+        sell: price[1],
+        lastUpdate: new Date(Date.now()).toUTCString(),
+    }
+    return gold
+}
+
+try {
+    const fName = './db.json'
+    let gold = await scrap()
+    fs.writeFile(fName, JSON.stringify(gold, undefined, 2))
+} catch (err) {
+    console.log(err)
+}
